f641d785364c4fcc0d1d5ac21c6372a226ab3db9
daimapi <unduainspacepig0302@gmail.com>
2024-06-01 02:11:19 +0800

    202406010210-freeRTOS for pms wake up

--- pms_and_bmp.ino
+++ pms_and_bmp.ino
@@ -7,90 +7,29 @@
 #include "ThingSpeak.h"
 #include "time.h"
 #include <ESP32Time.h>
-// SoftwareSerial Serial1(2, 3); // 將Arduino Pin2設定為RX, Pin3設定為TX
 ESP32Time rtc;
 PMS pms(Serial2);
 PMS::DATA data;
 Adafruit_BMP280 bmp;
 WiFiClient client;
 
-const char *ssid = "omg u sus";           // ssid:網路名稱
-const char *password = "uc2s33tuc465zix"; // pasword：網路密碼
+const char *ssid = "---";           // ssid:網路名稱
+const char *password = "---"; // pasword：網路密碼
 #define CHANNEL_ID 2544455
 #define CHANNEL_WRITE_API_KEY "3ZEDL3WPV2JS3NNN"
-//const int time_array[] = {7200, 72000, 50400, 28800};
 const int time_array[] = {7200, 72000, 50400, 28800};
+//const int time_array[] = {6240};
 ///////////////////////////////////////////////////////
-void setup()
-{
-  Serial.begin(9600);
-  Serial2.begin(9600); // for pms
-  esp_sleep_wakeup_cause_t wakeup_reason;
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-  Serial.print("hello");
-  Serial.println(wakeup_reason);
-  Serial.println(esp_reset_reason());
-  pms.passiveMode();
-
-  unsigned status = bmp.begin(0x76);
-  if (!status)
-  {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                     "try a different address!"));
-    while (1)
-      delay(10);
-  }
-  else
-  {
-    Serial.println("Good");
-  }
-  bmp.setSampling(Adafruit_BMP280::MODE_FORCED,   /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X1,   /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X1,   /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_OFF,    /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
-
-  pms.wakeUp();
-  delay(30000);
-
-  connectwifi();
-  ThingSpeak.begin(client);
-  Serial.println("o");
-  BMPreq();
-  PMSreq();
-  ThingSpeak.setField(8, WiFi.RSSI());
-  //ThingSpeak.writeFields(CHANNEL_ID, CHANNEL_WRITE_API_KEY);
-  if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER)
-  {
-    ThingSpeak.writeFields(CHANNEL_ID, CHANNEL_WRITE_API_KEY);
-    Serial.println("sent");
-    if (rtc.getHour(true) == 2)
-      NTPsync(); // sync every 02:00
-  }
-  else
-    NTPsync();
-
-  Serial.println("d");
-  int sleep_time = sleeptime();
-  if (sleep_time == 0){
-    sleep_time++;
-  }
-  Serial.print("byebye for : ");
-  Serial.println(sleep_time);
-  Serial.end();
-  esp_sleep_enable_timer_wakeup(sleep_time * 1000000ULL);
-  esp_deep_sleep_start();
-  //ESP.deepSleep(sleep_time * 1000000);
-}
-
 void connectwifi()
 {
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   Serial.println(String("Connecting to ") + ssid);
+  delay(100);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
   {
+    delay(10);
     Serial.print(".");
   }
   // how to sleep ???
@@ -102,6 +41,7 @@ void connectwifi()
     WiFi.printDiag(Serial); // 顯示WiFi連線狀態資訊：工作模式、Channel、SSID、Passphrase、BSSID
     Serial.println("WiFi RSSI:");
     Serial.println(WiFi.RSSI());
+    Serial.println("");
   }
   else
     Serial.println("connection failed");
@@ -181,12 +121,9 @@ void BMPreq()
     Serial.print(bmp.readPressure() / 100);
     Serial.println(" hPa");
     ThingSpeak.setField(6, round(bmp.readPressure() / 100));
-
     // Serial.print(F("Approx altitude = "));
     // Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
     // Serial.println(" m");
-
-    Serial.println();
   }
   else
   {
@@ -207,6 +144,78 @@ void NTPsync()
   // rtc.adjust(DateTime(t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec));
   Serial.print("NTPsynced : ");
   Serial.println(&t, "%A, %B %d %Y %H:%M:%S");
+}
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+  digitalWrite(2,LOW);
+  Serial.begin(9600);
+  Serial2.begin(9600); // for pms
+  delay(1000);
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  Serial.print("hello world : ");
+  Serial.println(wakeup_reason);
+  Serial.println(esp_reset_reason());
+  pms.passiveMode();
+
+  unsigned status = bmp.begin(0x76);
+  {
+    Serial.println("bmp waking up");
+    if (!status)
+    {
+      Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                       "try a different address!"));
+      while (1)
+        delay(10);
+    }
+    else
+    {
+      Serial.println("bmp init pass");
+      bmp.setSampling(Adafruit_BMP280::MODE_FORCED,   /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X1,   /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X1,   /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_OFF,    /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_1); /* Standby time. */
+    }
+  }
+
+  pms.wakeUp();
+  Serial.println("pms waking up");
+  delay(30000);
+  Serial.println("pms init pass");
+
+  connectwifi();
+  ThingSpeak.begin(client);
+  Serial.println("start process");
+  BMPreq();
+  PMSreq();
+  ThingSpeak.setField(8, WiFi.RSSI());
+  //ThingSpeak.writeFields(CHANNEL_ID, CHANNEL_WRITE_API_KEY);
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER)
+  {
+    ThingSpeak.writeFields(CHANNEL_ID, CHANNEL_WRITE_API_KEY);
+    Serial.println("sent");
+    if (rtc.getHour(true) == 2)
+      NTPsync(); // sync every 02:00
+  }
+  else
+  {
+    NTPsync();
+  }
+
+  Serial.println("end process");
+  int sleep_time = sleeptime();
+  if (sleep_time == 0){
+    sleep_time++;
+  }
+  Serial.print("byebye for : ");
+  Serial.println(sleep_time);
+  Serial.flush(); 
+  esp_sleep_enable_timer_wakeup(sleep_time * 1000000ULL);
+  esp_deep_sleep_start();
+  //ESP.deepSleep(sleep_time * 1000000);
 }
 
 void loop()
